ade011517532e849e53ccc50bdb415992f6f9c74
JabWik <jabwik@gmail.com>
2021-04-03 18:05:17 +0200

    added one-shot bullet

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 #Background
-background = pygame.image.load("space-background.jpg")
+#background = pygame.image.load("space-background.jpg") i
 
 #Title & Icon
 pygame.display.set_caption("Space Shooter")
@@ -30,11 +30,25 @@ enemyY = random.randint(10, 390)
 enemyX_change = 0.3
 enemyY_change = 40
 
+
+#Bullet
+bulletImg = pygame.image.load('bullet.png')
+bulletX = 0
+bulletY = 480
+bulletX_change = 0
+bulletY_change = 1
+bullet_state = "ready" #ready - invisible, fire - visible
+
 def player(x,y):
     screen.blit(playerImg,(x,y))
 
 def enemy(x,y):
     screen.blit(enemyImg,(x,y))
+
+def fire_bullet(x,y):
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
 
 #Game Loop
 running = True
@@ -42,7 +56,7 @@ while running:
 
     screen.fill((0, 100, 100))
 
-    screen.blit(background,(0,0))
+    #screen.blit(background,(0,0)) nie podoba mi się
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT: #to sprawia, że okno się nie zamyka
@@ -57,6 +71,8 @@ while running:
                 playerY_change = 0.25
             if event.key == pygame.K_UP:
                 playerY_change = -0.25
+            if event.key == pygame.K_SPACE:
+                fire_bullet(playerX,bulletY)
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
@@ -90,7 +106,10 @@ while running:
         enemyX_change = -0.3
         enemyY += enemyY_change
 
-
+    #bullet movement
+    if bullet_state is "fire":
+        fire_bullet(playerX,bulletY)
+        bulletY -= bulletY_change
 
     player(playerX, playerY)
     enemy(enemyX,enemyY)
